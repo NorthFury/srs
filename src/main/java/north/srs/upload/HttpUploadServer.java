7be621e4a7b57c18c051cdfe8868c71234ca4211
North <robyter@gmail>
2013-10-20 22:00:40 +0300

    minor change to HttpUploadServer

--- src/main/java/north/srs/upload/HttpUploadServer.java
+++ src/main/java/north/srs/upload/HttpUploadServer.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 The Netty Project
- *
- * The Netty Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 package north.srs.upload;
 
 import io.netty.bootstrap.ServerBootstrap;
@@ -24,11 +9,9 @@ import io.netty.handler.codec.http.HttpMethod;
 import north.srs.route.Matcher;
 import north.srs.route.Matchers;
 import north.srs.route.MethodMatcher;
+import north.srs.route.PathMatcher;
 import north.srs.route.Router;
 
-/**
- * A HTTP server showing how to use the HTTP multipart package for file uploads and decoding post data.
- */
 public class HttpUploadServer {
 
     private final int port;
@@ -59,9 +42,10 @@ public class HttpUploadServer {
 
     private Router createRouter() {
         Router router = new Router();
-        Matcher matcher = Matchers.or(
-                new MethodMatcher(HttpMethod.PUT),
-                new MethodMatcher(HttpMethod.POST));
+        Matcher matcher = Matchers.and(
+                new MethodMatcher(HttpMethod.GET),
+                new PathMatcher("/hello")
+        );
         router.addRoute(matcher, (request) -> {
             return "Hello";
         });
@@ -73,7 +57,7 @@ public class HttpUploadServer {
         if (args.length > 0) {
             port = Integer.parseInt(args[0]);
         } else {
-            port = 8085;
+            port = 8080;
         }
         if (args.length > 1) {
             isSSL = true;
